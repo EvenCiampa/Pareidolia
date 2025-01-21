62f444dd814d3e35a9fec1686d14cba3a49937d1
Even Ciampa <evenciampa12@gmail.com>
2025-01-21 11:30:58 +0100

    Fix api FE

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: false, //verrà utilizzata nell'ambiente di sviluppo
-	apiUrl: 'http://localhost:8080/api', //per le chiamate API generiche
+	apiUrl: 'http://localhost:8080', //per le chiamate API generiche
 	authEndpoint: 'http://localhost:8080/generic/access', //specifico per autenticazione
 };
